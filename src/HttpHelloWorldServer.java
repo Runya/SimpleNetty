b362754da93d39f062a4d55b56a1a48f8b9150ce
unknown <ruslan-2009-ua@yandex.ru>
2014-10-18 17:39:20 +0300

    conflict

--- src/HttpHelloWorldServer.java
+++ src/HttpHelloWorldServer.java
@@ -42,7 +42,7 @@ public final class HttpHelloWorldServer {
 
             Channel ch = b.bind(PORT).sync().channel();
 
-            System.err.println("Open your web browser and navigate to " +
+            System.err.println("Open your web browser and navigate to this page" +
                     (SSL? "https" : "http") + "://127.0.0.1:" + PORT + '/');
 
             ch.closeFuture().sync();
